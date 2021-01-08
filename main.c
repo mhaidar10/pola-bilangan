bb75b64d5419ce266b11c198ed24f674faf29975
mhaidar10 <72262185+mhaidar10@users.noreply.github.com>
2021-01-08 08:34:43 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int n,x,y;
+    printf("Masukan Baris = ");
+    scanf("%i",&n);
+     for (x=n;x>=1; x--)
+      {
+            for(y=1;y<=x;y++){
+                printf("%i",y);
+                if(y==x){
+                    printf("\n");
+                }
+            }
+
+      } */
+
+      /*int x,y,z;
+      printf("masukan bilangan = ");
+      scanf("%i",&x);
+      for(y=1;y<=x;y++){
+        for(z=1;z<=y;z++){
+            printf("%i",z);
+            if(z==y){
+            printf("\n");
+            }
+        }
+      } */
+
+
+    return 0;
+}
